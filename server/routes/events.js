8a57a9931b4cc923c3536e378d30a5d0251bedfb
John Tucker <john@larkintuckerllc.com>
2014-10-01 18:18:54 +0000

    fixed events on server - no delete and no add

--- server/routes/events.js
+++ server/routes/events.js
@@ -4,7 +4,5 @@ var events = require('../controllers/events');
 
 router.get('/', events.findAll);
 router.get('/:_id', events.findById);
-router.post('/', events.add);
-router.delete('/:_id', events.delete);
 
 module.exports = router;
